5aecf13b557e48d7d5f15b42e96b695f0fc1874c
Andrew McCluskey <andrew.mccluskey@ess.eu>
2022-02-01 16:09:43 +0100

    Update docs and plot and fitting

--- EasyReflectometry/fitting.py
+++ EasyReflectometry/fitting.py
@@ -7,11 +7,11 @@ class Fitter(easyFitter):
         self.calculator = calculator
         self.easy_f = easyFitter(model, calculator.fit_func)
 
-    def fit(self, data):
+    def fit(self, data, method='least_squares'):
         id = 0
         new_data = data.copy()
-        result = self.easy_f.fit(data.coords[f'Qz{id}'].values, data[f'data_set{id}'].data.values, weights=1/np.sqrt(data[f'data_set{id}'].data.variances), method='least_squares')
-        new_data[f'data_set{id}_best_fit'] = sc.array(dims=[f'Qz{id}'], values=self.calculator.fit_func(data.coords[f'Qz{id}'].values))
-        new_data[f'data_set{id}_sld'] = sc.array(dims=[f'z{id}'], values=self.calculator.sld_profile()[1] * 1e-6, unit=sc.Unit('1/angstrom') ** 2)
+        result = self.easy_f.fit(data.coords[f'Qz{id}'].values, data[f'R{id}'].data.values, weights=1/np.sqrt(data[f'R{id}'].data.variances), method=method)
+        new_data[f'R{id}_model'] = sc.array(dims=[f'Qz{id}'], values=self.calculator.fit_func(data.coords[f'Qz{id}'].values))
+        new_data[f'SLD{id}'] = sc.array(dims=[f'z{id}'], values=self.calculator.sld_profile()[1] * 1e-6, unit=sc.Unit('1/angstrom') ** 2)
         new_data.coords[f'z{id}'] = sc.array(dims=[f'z{id}'], values=self.calculator.sld_profile()[0], unit=(1/new_data.coords[f'Qz{id}'].unit).unit)
         return new_data
--- EasyReflectometry/plot.py
+++ EasyReflectometry/plot.py
@@ -12,7 +12,7 @@ def plot(data: sc.Dataset) -> ipympl.backend_nbagg.Canvas:
     
     :returns: The plot canvas.
     """
-    if len([i for i in list(data.keys()) if 'sld' in i]) == 0:
+    if len([i for i in list(data.keys()) if 'SLD' in i]) == 0:
         fig = plt.figure(figsize=(5, 3))
         gs = GridSpec(1, 1, figure=fig)
     else:
@@ -22,12 +22,12 @@ def plot(data: sc.Dataset) -> ipympl.backend_nbagg.Canvas:
     ax1 = fig.add_subplot(gs[0, 0])
     refl_nums = [k[2:] for k, v in data.coords.items() if 'Qz' == k[:2]]
     for i in refl_nums:
-        sc.plot(data[f'data_set{i}'], ax=ax1, norm='log', linestyle='', marker='o')
+        sc.plot(data[f'R{i}'], ax=ax1, norm='log', linestyle='', marker='.')
         try:
-            sc.plot(data[f'data_set{i}_best_fit'], ax=ax1, norm='log', linestyle='-', color='orange', marker='')
+            sc.plot(data[f'R{i}_model'], ax=ax1, norm='log', linestyle='-', color='orange', marker='')
         except sc.NotFoundError:
             pass
     sld_nums = [k[1:] for k, v in data.coords.items() if 'z' == k[0]]
     for i in sld_nums:
-        sc.plot(data[f'data_set{i}_sld'], ax=ax2, linestyle='-', color='orange', marker='')
+        sc.plot(data[f'SLD{i}'], ax=ax2, linestyle='-', color='orange', marker='')
     return fig.canvas
