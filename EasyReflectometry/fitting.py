54eab89a75c0bac8c8cc433c07ad8cc5d971c7c1
Andrew McCluskey <andrew.mccluskey@ess.eu>
2022-02-02 15:34:35 +0100

    Documentaiton and a test for the convincence function for fitting

--- EasyReflectometry/fitting.py
+++ EasyReflectometry/fitting.py
@@ -5,11 +5,23 @@ from easyCore.Fitting.Fitting import Fitter as easyFitter
 
 class Fitter(easyFitter):
 
-    def __init__(self, model, calculator):
-        self.calculator = calculator
-        self.easy_f = easyFitter(model, calculator.fit_func)
+    def __init__(self, model: "EasyReflectometry.experiment.model.Model", interface: "EasyReflectometry.interface.InterfaceFactory"):
+        """
+        A convinence class for the :py:class:`easyCore.Fitting.Fitting` which will populate the :py:class:`sc.Dataset` appropriately after the fitting is performed. 
 
-    def fit(self, data, method='least_squares'):
+        :param model: Reflectometry model
+        :param interface: Analysis interface
+        """
+        self.interface = interface
+        self.easy_f = easyFitter(model, interface.fit_func)
+
+    def fit(self, data: sc.Dataset, method: str='least_squares'):
+        """
+        Perform the fitting and populate the datasets with the result.
+        
+        :param data: Dataset to be fitted to and populated
+        :param method: Optimisation method
+        """
         id = 0
         new_data = data.copy()
         result = self.easy_f.fit(data.coords[f'Qz{id}'].values,
@@ -17,15 +29,15 @@ class Fitter(easyFitter):
                                  weights=1 / np.sqrt(data[f'R{id}'].data.variances),
                                  method=method)
         new_data[f'R{id}_model'] = sc.array(dims=[f'Qz{id}'],
-                                            values=self.calculator.fit_func(
+                                            values=self.interface.fit_func(
                                                 data.coords[f'Qz{id}'].values))
         new_data[f'SLD{id}'] = sc.array(dims=[f'z{id}'],
-                                        values=self.calculator.sld_profile()[1] * 1e-6,
+                                        values=self.interface.sld_profile()[1] * 1e-6,
                                         unit=sc.Unit('1/angstrom')**2)
         new_data[f'R{id}_model'].attrs['model'] = sc.scalar(
             self.easy_f._fit_object.as_dict())
         new_data.coords[f'z{id}'] = sc.array(
             dims=[f'z{id}'],
-            values=self.calculator.sld_profile()[0],
+            values=self.interface.sld_profile()[0],
             unit=(1 / new_data.coords[f'Qz{id}'].unit).unit)
         return new_data
---
+++ tests/test_fitting.py
@@ -0,0 +1,55 @@
+__author__ = 'github.com/arm61'
+"""
+Tests for fitting module
+"""
+
+import os
+import unittest
+import numpy as np
+from numpy.testing import assert_almost_equal
+import scipp as sc
+from orsopy.fileio import load_orso, Header
+import EasyReflectometry
+from EasyReflectometry.sample import Material, Layer, Structure
+from EasyReflectometry.experiment.model import Model
+from EasyReflectometry.data import load
+from EasyReflectometry.interface import InterfaceFactory as Interface
+from EasyReflectometry.fitting import Fitter
+
+
+class TestFitting(unittest.TestCase):
+    def test_fitting(self):
+        fpath = os.path.join(
+                os.path.dirname(os.path.dirname(EasyReflectometry.__file__)),
+                'tests/_static/example.ort')
+        data = load(fpath)
+        si = Material.from_pars(2.07, 0, 'Si')
+        sio2 = Material.from_pars(3.47, 0, 'SiO2')
+        film = Material.from_pars(2.0, 0, 'Film')
+        d2o = Material.from_pars(6.36, 0, 'D2O')
+        si_layer = Layer.from_pars(si, 0, 0, 'Si layer')
+        sio2_layer = Layer.from_pars(sio2, 30, 3, 'SiO2 layer')
+        film_layer = Layer.from_pars(film, 250, 3, 'Film Layer')
+        superphase = Layer.from_pars(d2o, 0, 3, 'D2O Subphase')
+        structure = Structure.from_pars(si_layer, sio2_layer, film_layer, superphase, name='Film Structure')
+        model = Model.from_pars(structure, 1, 1e-6, 0.02, 'Film Model')
+        # Thicknesses
+        sio2_layer.thickness.bounds = (15, 50)
+        film_layer.thickness.bounds = (200, 300)
+        # Roughnesses
+        sio2_layer.roughness.bounds = (1, 15)
+        film_layer.roughness.bounds = (1, 15)
+        superphase.roughness.bounds = (1, 15)
+        # Scattering length density
+        film.sld.bounds = (0.1, 3)
+        # Background
+        model.background.bounds = (1e-8, 1e-5)
+        # Scale
+        model.scale.bounds = (0.5, 1.5)
+        interface = Interface()
+        model.interface = interface 
+        fitter = Fitter(model, interface)
+        assert fitter.interface == interface
+        analysed = fitter.fit(data)
+        assert 'R0_model' in analysed.keys()
+        assert 'SLD0' in analysed.keys()
