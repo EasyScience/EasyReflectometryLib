4d828fbc343f786852ee5bb2ae483e7d9145eeaf
Andrew McCluskey <andrew.mccluskey@ess.eu>
2022-02-02 09:13:22 +0100

    Add the repeating multilayer tutorial

--- EasyReflectometry/fitting.py
+++ EasyReflectometry/fitting.py
@@ -13,5 +13,6 @@ class Fitter(easyFitter):
         result = self.easy_f.fit(data.coords[f'Qz{id}'].values, data[f'R{id}'].data.values, weights=1/np.sqrt(data[f'R{id}'].data.variances), method=method)
         new_data[f'R{id}_model'] = sc.array(dims=[f'Qz{id}'], values=self.calculator.fit_func(data.coords[f'Qz{id}'].values))
         new_data[f'SLD{id}'] = sc.array(dims=[f'z{id}'], values=self.calculator.sld_profile()[1] * 1e-6, unit=sc.Unit('1/angstrom') ** 2)
+        new_data[f'R{id}_model'].attrs['model'] = sc.scalar(self.easy_f._fit_object.as_dict())
         new_data.coords[f'z{id}'] = sc.array(dims=[f'z{id}'], values=self.calculator.sld_profile()[0], unit=(1/new_data.coords[f'Qz{id}'].unit).unit)
         return new_data
