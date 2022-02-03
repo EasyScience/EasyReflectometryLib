ded99ce28b944d4de42872dd3191049ff0140216
Andrew McCluskey <andrew.mccluskey@ess.eu>
2022-02-03 11:54:03 +0100

    Move some files around

--- EasyReflectometry/sample/__init__.py
+++ EasyReflectometry/sample/__init__.py
@@ -1,6 +1,4 @@
-from .material import Material
 from .layer import Layer
 from .layers import Layers
-from .material import Material
 from .materials import Materials
 from .structure import Structure
