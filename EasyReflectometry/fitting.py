21c61c4d26491e1c896aba68e3c6aa0c09424ec4
Andrew McCluskey <andrew.mccluskey@ess.eu>
2022-03-06 12:26:52 +0100

    Fixes to the fitting to better support single and multiple contrasts

--- EasyReflectometry/fitting.py
+++ EasyReflectometry/fitting.py
@@ -39,25 +39,42 @@ class Fitter:
         dy = [1 / np.sqrt(data[f'R_{i}'].data.variances) for i in refl_nums]
         if len(x) > 1:
             result = self.easy_f.fit_lists(x, y, weights_list=dy, method=method)
+            new_data = data.copy()
+            for i, x in enumerate(result.x):
+                id = refl_nums[i]
+                new_data[f'R_{id}_model'] = sc.array(dims=[f'Qz_{id}'],
+                                                    values=self.easy_f._fit_functions[i](
+                                                        data.coords[f'Qz_{id}'].values))
+                new_data[f'SLD_{id}'] = sc.array(
+                    dims=[f'z_{id}'],
+                    values=self.easy_f._fit_objects[i].interface.sld_profile()[1] * 1e-6,
+                    unit=sc.Unit('1/angstrom')**2)
+                new_data[f'R_{id}_model'].attrs['model'] = sc.scalar(
+                    self.easy_f._fit_objects[i].as_dict())
+                new_data.coords[f'z_{id}'] = sc.array(
+                    dims=[f'z_{id}'],
+                    values=self.easy_f._fit_objects[i].interface.sld_profile()[0],
+                    unit=(1 / new_data.coords[f'Qz_{id}'].unit).unit)
+            return new_data
         else:
             result = self.easy_f.fit(x[0], y[0], weights=dy[0], method=method)
-        new_data = data.copy()
-        for i, x in enumerate(result.x):
-            id = refl_nums[i]
+            new_data = data.copy()
+            id = refl_nums[0]
             new_data[f'R_{id}_model'] = sc.array(dims=[f'Qz_{id}'],
-                                                 values=self.easy_f._fit_functions[i](
+                                                 values=self.easy_f._fit_function(
                                                      data.coords[f'Qz_{id}'].values))
             new_data[f'SLD_{id}'] = sc.array(
                 dims=[f'z_{id}'],
-                values=self.easy_f._fit_objects[i].interface.sld_profile()[1] * 1e-6,
+                values=self.easy_f._fit_object.interface.sld_profile()[1] * 1e-6,
                 unit=sc.Unit('1/angstrom')**2)
             new_data[f'R_{id}_model'].attrs['model'] = sc.scalar(
                 self.easy_f._fit_object.as_dict())
             new_data.coords[f'z_{id}'] = sc.array(
                 dims=[f'z_{id}'],
-                values=self.easy_f._fit_objects[i].interface.sld_profile()[0],
+                values=self.easy_f._fit_object.interface.sld_profile()[0],
                 unit=(1 / new_data.coords[f'Qz_{id}'].unit).unit)
-        return new_data
+            return new_data
+        
 
 
 def _flatten_list(this_list: list) -> list:
