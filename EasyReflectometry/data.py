3cb9c1a79f0443266e594d21a6e72e343481310d
Andreas Pedersen <andped10@gmail.com>
2024-02-01 08:30:06 +0100

    back to code with no issues

--- EasyReflectometry/data.py
+++ EasyReflectometry/data.py
@@ -1,4 +1,4 @@
-__author__ = 'github.com/arm61'
+__author__ = "github.com/arm61"
 
 from typing import (
     TextIO,
@@ -8,7 +8,8 @@ from typing import (
 import numpy as np
 import scipp as sc
 from orsopy.fileio import (
-    Header, orso,
+    Header,
+    orso,
 )
 
 
@@ -42,26 +43,26 @@ def _load_orso(fname: Union[TextIO, str]) -> sc.DataGroup:
         name = i
         if o.info.data_set is not None:
             name = o.info.data_set
-        coords[f'Qz_{name}'] = sc.array(
-            dims=[f'{o.info.columns[0].name}_{name}'],
+        coords[f"Qz_{name}"] = sc.array(
+            dims=[f"{o.info.columns[0].name}_{name}"],
             values=o.data[:, 0],
             variances=np.square(o.data[:, 3]),
-            unit=sc.Unit(o.info.columns[0].unit)
+            unit=sc.Unit(o.info.columns[0].unit),
         )
         try:
-            data[f'R_{name}'] = sc.array(
-                dims=[f'{o.info.columns[0].name}_{name}'],
+            data[f"R_{name}"] = sc.array(
+                dims=[f"{o.info.columns[0].name}_{name}"],
                 values=o.data[:, 1],
                 variances=np.square(o.data[:, 2]),
-                unit=sc.Unit(o.info.columns[1].unit)
+                unit=sc.Unit(o.info.columns[1].unit),
             )
         except TypeError:
-            data[f'R_{name}'] = sc.array(
-                dims=[f'{o.info.columns[0].name}_{name}'],
+            data[f"R_{name}"] = sc.array(
+                dims=[f"{o.info.columns[0].name}_{name}"],
                 values=o.data[:, 1],
-                variances=np.square(o.data[:, 2])
+                variances=np.square(o.data[:, 2]),
             )
-        attrs[f'R_{name}'] = {'orso_header': sc.scalar(Header.asdict(o.info))}
+        attrs[f"R_{name}"] = {"orso_header": sc.scalar(Header.asdict(o.info))}
     return sc.DataGroup(data=data, coords=coords, attrs=attrs)
 
 
@@ -75,16 +76,16 @@ def _load_txt(fname: Union[TextIO, str]) -> sc.DataGroup:
     """
     f_data = np.loadtxt(fname)
     data = {
-        'R_0':
-        sc.array(dims=['Qz_0'], values=f_data[:, 1], variances=np.square(f_data[:, 2]))
+        "R_0": sc.array(
+            dims=["Qz_0"], values=f_data[:, 1], variances=np.square(f_data[:, 2])
+        )
     }
     coords = {
-        data['R_0'].dims[0]:
-        sc.array(
-            dims=['Qz_0'],
+        data["R_0"].dims[0]: sc.array(
+            dims=["Qz_0"],
             values=f_data[:, 0],
             variances=np.square(f_data[:, 3]),
-            unit=sc.Unit('1/angstrom')
+            unit=sc.Unit("1/angstrom"),
         )
     }
     return sc.DataGroup(data=data, coords=coords)
