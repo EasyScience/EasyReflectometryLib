aadcb7fc9a6f28fb150d8eb5548ecbce42cecf25
Andreas Pedersen <andped10@gmail.com>
2024-02-28 07:02:39 +0100

    ruff

--- docs/conf.py
+++ docs/conf.py
@@ -17,9 +17,9 @@
 # relative to the documentation root, use os.path.abspath to make it
 # absolute, like shown here.
 #
+import datetime
 import os
 import sys
-import datetime
 
 sys.path.insert(0, os.path.abspath('..'))
 
