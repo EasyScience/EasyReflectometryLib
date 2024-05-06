370d515adfad2da15c6211d154e5a59f6bc44d73
Andreas Pedersen <andreas.pedersen@ess.eu>
2024-05-06 14:47:31 +0200

    conf.py

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -23,7 +23,7 @@ import sys
 import toml
 from pathlib import Path
 
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath('../src'))
 
 main_root = Path(__file__).parents[2]
 sys.path.append(str(main_root))
@@ -116,7 +116,6 @@ html_theme_options = {
     'navigation_with_keys': True
 }
 html_baseurl = 'https://docs.easyreflectometry.org'
-
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
 # documentation.
