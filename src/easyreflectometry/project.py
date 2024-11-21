01f8adb7f0bc1bff793c0a2ce30465af281c35ea
Andreas Pedersen <andreas.pedersen@ess.eu>
2024-11-21 10:40:42 +0100

    CodeFactor

--- src/easyreflectometry/project.py
+++ src/easyreflectometry/project.py
@@ -454,7 +454,7 @@ class Project:
     def free_parameters_count(self) -> int:
         count = 0
         for parameter in self.parameters:
-            if parameter.free is True:
+            if parameter:
                 count = count + 1
         return count
 
